5ad06717873bbacff45cdae56062c2f2774ed299
Collin Jackson <jackson@google.com>
2015-07-01 12:33:12 -0700

    fix tests another way

--- sky/sdk/lib/widgets/material.dart
+++ sky/sdk/lib/widgets/material.dart
@@ -16,7 +16,7 @@ const Map<MaterialType, double> edges = const {
   MaterialType.canvas: null,
   MaterialType.card: 2.0,
   MaterialType.circle: null,
-  MaterialType.custom: null,
+  MaterialType.button: 2.0,
 };
 
 const double _kAnimateShadowDurationMS = 100.0;
