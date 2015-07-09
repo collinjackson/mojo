2d38522e1fd517f78fae3eb5b3932f0262dba063
Collin Jackson <jackson@google.com>
2015-07-09 12:50:15 -0700

    remove suffix

--- sky/sdk/example/fitness/lib/settings.dart
+++ sky/sdk/example/fitness/lib/settings.dart
@@ -36,7 +36,7 @@ class SettingsFragment extends Component {
   Widget buildToolBar() {
     return new ToolBar(
       left: new IconButton(
-        icon: "navigation/arrow_back_${Theme.of(this).toolbarIconSuffix}",
+        icon: "navigation/arrow_back",
         onPressed: navigator.pop),
       center: new Text('Settings')
     );
